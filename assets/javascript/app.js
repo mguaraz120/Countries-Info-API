c37866ad814c7e6d40e63ba06cbb3ec00343c98e
sebas0001 <guarazs@yahoo.com.ar>
2020-05-12 18:34:14 -0300

    Added 2nd api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@ $(window).on("load", () => {
     let long;
     let lat;
     let date;
+    let pais= "colombia";
+  
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) => {
             console.log(position)
@@ -13,9 +15,16 @@ $(window).on("load", () => {
 
             var settings = {
                 url: `https://api.climacell.co/v3/weather/realtime?fields=precipitation,temp,feels_like,visibility,humidity,wind_direction&apikey=ipXtA13qS9UyzFJGU4B0C7xC3VHy52Cw&lat=${lat}&lon=${long}`,
-                method: "GET",
+                method: "GET"
          
               };
+
+              var country = {
+                url: `https://restcountries.eu/rest/v2/name/${pais}?fullText=true`,
+                method: "GET"
+         
+              };
+
 
             $.ajax(settings).done((response) => {
                 console.log(response);
@@ -29,6 +38,20 @@ $(window).on("load", () => {
                 tempDiv.append(tempP, TermicaP, humedad, visibilidad);
                 $(".clima").append(tempDiv);
                 $(".hora").append(date);
+
+            })
+
+            $.ajax(country).done((response) => {
+                console.log(response);
+                const{name, capital, subregion, population} = response[0];
+                var paisDiv = $("<div>");
+                var paisNameDiv = $("<p>").text(name);
+                var capitalDiv = $("<p>").text(`Capital: ${capital}`);
+                var regionDiv = $("<p>").text(`Region: ${subregion}`);
+                var populationDiv = $("<p>").text(`Poblacion: ${population}`);
+
+                paisDiv.append(paisNameDiv, capitalDiv, regionDiv, populationDiv);
+                $(".pais").append(paisDiv);
 
             })
 
